302ff333e361288d48d52014e5d5f3194b4b7881
ashuk1109 <ashutoshkarwa123@gmail.com>
2021-02-07 14:54:13 +0530

    RMX2001: Fix OK Google Detection * Also add libshowlogo blob

--- RMX2001-vendor.mk
+++ RMX2001-vendor.mk
@@ -21,9 +21,11 @@ PRODUCT_COPY_FILES += \
     vendor/realme/RMX2001/proprietary/product/etc/apns-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml \
     vendor/realme/RMX2001/proprietary/bin/kpoc_charger:$(TARGET_COPY_OUT_SYSTEM)/bin/kpoc_charger \
     vendor/realme/RMX2001/proprietary/etc/init/kpoc_charger.rc:$(TARGET_COPY_OUT_SYSTEM)/etc/init/kpoc_charger.rc \
+    vendor/realme/RMX2001/proprietary/lib/libshowlogo.so:$(TARGET_COPY_OUT_SYSTEM)/lib/libshowlogo.so \
     vendor/realme/RMX2001/proprietary/lib64/libaptXHD_encoder.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libaptXHD_encoder.so \
     vendor/realme/RMX2001/proprietary/lib64/libaptX_encoder.so:$(TARGET_COPY_OUT_SYSTEM)/lib64/libaptX_encoder.so \
     vendor/realme/RMX2001/proprietary/product/vendor_overlay/29/lib/libvpud_vcodec.so:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/29/lib/libvpud_vcodec.so
 
 PRODUCT_PACKAGES += \
-    ImsService 
+    ImsService \
+    HotwordEnrollmentOKGoogleCORTEXM4
